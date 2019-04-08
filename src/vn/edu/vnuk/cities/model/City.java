e2b90d9aafaec4754ac70c8f07d4135d92542a2a
lethithuhuong99 <huong.le170205@vnuk.edu.vn>
2019-04-08 22:51:44 +0700

    08/04/2019

---
+++ src/vn/edu/vnuk/cities/model/City.java
@@ -0,0 +1,37 @@
+package vn.edu.vnuk.cities.model;
+
+public class City {
+	private Long id;
+	private String city;
+	private String province;
+	private Long zipCode;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getProvince() {
+		return province;
+	}
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	public Long getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(Long zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+	
+	
+}
---
+++ src/vn/edu/vnuk/cities/sql/Sql5010InsertIntoCities.java
@@ -0,0 +1,53 @@
+package vn.edu.vnuk.cities.sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Sql5010InsertIntoCities {
+	
+	private final Connection connection;
+	private final String sqlQuery;
+	
+	public Sql5010InsertIntoCities(Connection connection) {
+		this.connection = connection;
+		
+		this.sqlQuery = "insert into cities (city, province, zip_code) "
+                +	"values (?, ?, ?)";
+	}
+	
+	public void run() throws SQLException {
+
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println(">  Sql5010InsertIntoCategories started");
+		
+		PreparedStatement statement;
+		
+		try {
+			statement = connection.prepareStatement(sqlQuery);
+
+            //	Replacing "?" through values
+            statement.setString(1, "Da Nang");
+            statement.setString(2, "Da Nang");
+            statement.setLong(3, 84);
+
+            // 	Executing statement
+			statement.execute();
+			statement.close();
+	        System.out.println("   DATA successfully loaded in \'categories\'");
+		
+		}
+		
+		catch (Exception e) {
+	        e.printStackTrace();
+	        connection.close();
+		}
+		
+		finally {
+			System.out.println("<  Sql5010InsertIntoCategories ended");
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			System.out.println("");
+		}
+			
+	}
+}
